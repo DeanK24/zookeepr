cb51a06593383d1555ba4d2eb57d81811b635087
Dean <deank242@gmail.com>
2022-05-04 13:45:10 -0400

    finish post request

--- server.js
+++ server.js
@@ -116,9 +116,15 @@ app.post('/api/animals', (req, res) => {
   // req.body is where our incoming content will be 
   req.body.id = animals.length.toString();
 
-  //add animal to json file and animals array in this function
-  const animal = createNewAnimal(req.body, animals);
-  res.json(animal);
+  //if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+
+    res.json(animal);
+  }
+  
 });
 
 app.listen(PORT, () => {
